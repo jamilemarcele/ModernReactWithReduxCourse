8d0ce1ecdcd09bf42f320f9c4a7c15a2271ac344
Jamile Radloff <jamile.radloff@gmail.com>
2021-04-07 16:39:55 +0200

    Rebuild youtube videos app with hooks (#3)
    * first commit
    
    * refactored videos app using hooks and created a custom hooks

---
+++ videos-hooks/src/components/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
+
+const App = () => {
+    const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('beatles');
+
+    useEffect(() => {
+        setSelectedVideo(videos[0]);
+    }, [videos]);
+
+    return (
+        <div className="ui container">
+            <SearchBar onFormSubmit={search} />
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList
+                             // Code in line 38 is the same as: (video) => setSelectedVideo(video)
+                            onVideoSelect={setSelectedVideo}
+                            videos={videos}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
